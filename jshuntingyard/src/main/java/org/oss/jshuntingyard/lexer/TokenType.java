bb55f0c62154fa79ee59303b55f7cc4a7a3f2aef
Magnus Karlsson <mkarlsson@open-software-solutions.ch>
2015-07-06 19:43:04 +0200

    case-insensitive matching

--- jshuntingyard/src/main/java/org/oss/jshuntingyard/lexer/TokenType.java
+++ jshuntingyard/src/main/java/org/oss/jshuntingyard/lexer/TokenType.java
@@ -3,8 +3,8 @@ package org.oss.jshuntingyard.lexer;
 import java.util.regex.Pattern;
 
 public enum TokenType {
-	BOOLEANVALUE("false|true|False|True|FALSE|TRUE"),
-	NULL("null|Null|NULL"),
+	BOOLEANVALUE("(?i)false|(?i)true"),
+	NULL("(?i)null"),
 	NAN("NaN"),
 	FUNCTIONNAME("[A-Za-z]+([0-9])?"),
 	DIGIT("([-])?([0-9]+(\\.\\d+)?)"),
@@ -14,13 +14,13 @@ public enum TokenType {
 	CLOSEBRACE("\\)"),
 	OPERATOR("\\+|-|\\*|/|==|!=|\\^|\\%|\\|\\||<=|>=|<|>|&&|!"),
 	VARIABLE("\\$([a-zA-Z0-9])+([_])?([a-zA-Z0-9])*");
-	
+
 	private final Pattern pattern;
-	
+
 	TokenType(String regularExpression) {
 		this.pattern = Pattern.compile(regularExpression);
 	}
-	
+
 	public Pattern pattern() {
 		return pattern;
 	}
