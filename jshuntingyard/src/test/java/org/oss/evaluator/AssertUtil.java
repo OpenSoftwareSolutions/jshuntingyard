fafce1294a75184b51897e83b4a0fcabd3eefe1d
Donat Mueller <dmueller@open-software-solutions.ch>
2015-08-28 18:18:38 +0200

    primitive operation support for new types

--- jshuntingyard/src/test/java/org/oss/evaluator/AssertUtil.java
+++ jshuntingyard/src/test/java/org/oss/evaluator/AssertUtil.java
@@ -29,25 +29,25 @@ public class AssertUtil {
 
 
 	static void assertBooleanResult(FunctionElementArgument<?> result, Boolean expected) {
-		assertTrue("BooleanArgument expected",result instanceof BooleanArgument);
+		assertTrue("BooleanArgument expected and not " + result.getClass().getSimpleName(),result instanceof BooleanArgument);
 		BooleanArgument boolResult = (BooleanArgument) result;
 		assertTrue("Value of " + expected + " expected." ,boolResult.getValue() == expected);
 	}
 
 	static void assertDoubleResult(FunctionElementArgument<?> result, Double expected) {
-		assertTrue("DoubleArgument expected",result instanceof DoubleArgument);
+		assertTrue("DoubleArgument expected and not " + result.getClass().getSimpleName(),result instanceof DoubleArgument);
 		DoubleArgument doubleResult = (DoubleArgument) result;
 		assertTrue("Value of " + expected + " expected and not " + doubleResult ,doubleResult.getValue().equals(expected));
 	}
 
 	static void assertIntegerResult(FunctionElementArgument<?> result, Integer expected) {
-		assertTrue("IntegerArgument expected",result instanceof IntegerArgument);
+		assertTrue("IntegerArgument expected and not " + result.getClass().getSimpleName(),result instanceof IntegerArgument);
 		IntegerArgument integerResult = (IntegerArgument) result;
 		assertTrue("Value of " + expected + " expected and not " + integerResult ,integerResult.getValue().equals(expected));
 	}
 
 	static void assertStringResult(FunctionElementArgument<?> result, String expected) {
-		assertTrue("StringArgument expected",result instanceof StringArgument);
+		assertTrue("StringArgument expected and not " + result.getClass().getSimpleName(),result instanceof StringArgument);
 		StringArgument stringResult = (StringArgument) result;
 		assertTrue("Value of " + expected + " expected and not " + stringResult.getValue()  ,stringResult.getValue().equals(expected));
 	}
